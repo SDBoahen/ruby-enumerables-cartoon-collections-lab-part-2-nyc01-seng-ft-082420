b603e976b271db8ff242cc2fcb373536c33f666c
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-22 01:10:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,8 @@ def
   
   summon_captain_planet(planeteer_calls)
   
+  
+  
 end
 # Use an Enumerable to capitalize and add '!' to every element in the passed in array
 # Return a new array of the results
